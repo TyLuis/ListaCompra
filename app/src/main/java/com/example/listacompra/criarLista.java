ea211f6a428128b0a6d016254651dd0292a87881
Luís Augusto Borges <52267200+TyLuis@users.noreply.github.com>
2022-06-27 22:04:13 -0300

    Initial commit

---
+++ app/src/main/java/com/example/listacompra/criarLista.java
@@ -0,0 +1,14 @@
+package com.example.listacompra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class criarLista extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_criar_lista);
+    }
+}
