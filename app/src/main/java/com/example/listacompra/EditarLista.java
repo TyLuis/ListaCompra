e33622258d3e65ab352439866887c1e6d0b59548
Luís Augusto Borges <52267200+TyLuis@users.noreply.github.com>
2022-06-29 21:37:24 -0300

    Começo tela editar
    Componentes da tela de criação posicionados, inicializado os componentes da tela de edição

---
+++ app/src/main/java/com/example/listacompra/EditarLista.java
@@ -0,0 +1,14 @@
+package com.example.listacompra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditarLista extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_lista);
+    }
+}
